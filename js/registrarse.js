c366175b330f7b7c0c39bc9254a2f75e003bd44f
Elpancho <santiagosanchez,ribero@gmail.com>
2024-05-12 15:24:37 -0500

    feat: :sparkles: Correccion de rutas a rutas alternativas '../'

--- js/registrarse.js
+++ js/registrarse.js
@@ -17,5 +17,5 @@ document.getElementById('realizar_registro').addEventListener('click',function(e
 })
 
 document.getElementById('volver_inicio').addEventListener('click', function(){
-    window.location.href='/index.html';
+    window.location.href='../index.html';
 })
