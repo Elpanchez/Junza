72448bed71ac7b18962dfd78425bb5137c79c5a2
Elpanchez <santiagosanchez.ribero@gmail.com>
2024-05-21 01:17:39 -0500

    feat: :sparkles: Botón home en la vista foro para volver a la vista interfaz
    Se agregó un elemento button junto con la funcionalidad y estilos

--- js/foro.js
+++ js/foro.js
@@ -111,3 +111,7 @@ document.addEventListener('DOMContentLoaded', () => {
     loadQuestions();
     loadResponses();
 });
+
+document.getElementById('home').addEventListener('click', function() {
+    window.location.href = 'interfaz.html';
+});
